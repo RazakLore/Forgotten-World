53e7f377d550c07f80d880f2281646de213ac92b
Dylan <AlusKielith@gmail.com>
2025-09-26 14:59:49 +0100

    Player moves across scenes
    Next task is to get the player to spawn at the appropriate ID game object location on scene load

--- Assets/Scripts/AreaConnector.cs
+++ Assets/Scripts/AreaConnector.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class AreaConnector : MonoBehaviour
 {
@@ -14,7 +15,33 @@ public class AreaConnector : MonoBehaviour
         {
             // Send the targetID to the game manager
 
-            SceneManager.LoadScene(targetScene);
+            //SceneManager.MoveGameObjectToScene(collision.gameObject, SceneManager.GetSceneByName(targetScene));
+            StartCoroutine(LoadYourAsyncScene(collision.gameObject));
+            //SceneManager.LoadScene(targetScene);
+            collision.gameObject.transform.position = GameObject.Find(targetID + "Spawn").transform.position;
         }
+    }
+
+    IEnumerator LoadYourAsyncScene(GameObject player)
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(targetScene, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByName(targetScene));
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+
+        PlayerPrefs.SetString("SCENE_SPAWN_POINT", targetScene);
+        //player.transform.position = GameObject.Find(targetID + "Spawn").transform.position; // DOESNT WORK
     }
 }
