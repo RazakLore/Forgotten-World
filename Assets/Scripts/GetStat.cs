34b26f5f87789ad003ddda0acbb6fd24ca8d2bb3
Dylan <AlusKielith@gmail.com>
2025-11-07 14:56:14 +0000

    UI Updates

---
+++ Assets/Scripts/GetStat.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GetStat : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public grabthestat()
+    {
+        // call this whenever i open the pauise menu or need to update in battle
+
+    }
+}
